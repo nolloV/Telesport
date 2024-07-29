58776f2168baa2bb461dfcc873058cee17434802
nolloV <142914191+nolloV@users.noreply.github.com>
2024-07-29 11:44:45 -0400

    add models & services

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,24 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<Olympic[] | undefined>(undefined); //stocke et émet les données olympiques.
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {} //permet les requêtes HTTP.
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe( //Effectue une requête GET pour obtenir les données 
+      tap((value) => this.olympics$.next(value)), //Met à jour olympics$ avec les données reçues.
       catchError((error, caught) => {
         // TODO: improve error handling
-        console.error(error);
+        console.error('An error occurred:', error);
         // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
+        alert('An error occurred while loading the data. Please try again later.');
+        this.olympics$.next(undefined);
         return caught;
       })
     );
